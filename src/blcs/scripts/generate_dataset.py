fec9fc7b7edacedf3a04390fa632ccaf4e66c338
Motoki0705 <kamimura098@gmail.com>
2025-12-29 11:29:06 +0900

    fix import path for CameraConfig in generate_dataset.py

--- src/blcs/scripts/generate_dataset.py
+++ src/blcs/scripts/generate_dataset.py
@@ -24,7 +24,7 @@ import torch
 from hydra.utils import to_absolute_path
 
 from src.blcs.generate_dataset.io.dataset_io import BLCSDatasetWriter
-from src.blcs.generate_dataset.projection.camera_projector import CameraConfig
+from src.utils.projection.camera_projector import CameraConfig
 from src.blcs.generate_dataset.sampling.distribution_sampler import SamplingConfig
 from src.blcs.generate_dataset.scene_generator import BLCSSceneGenerator, GeneratorConfig
 from src.blcs.simulation.ball_physics import PhysicsConfig
