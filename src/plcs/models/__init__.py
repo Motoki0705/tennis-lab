7cb263a9f39fc13a7989d9f8fe2beb495f06e769
Motoki0705 <kamimura098@gmail.com>
2026-02-09 23:21:54 +0900

    Add query-based PLCS sequence model and wire sequence trainer

--- src/plcs/models/__init__.py
+++ src/plcs/models/__init__.py
@@ -7,11 +7,17 @@ from src.plcs.models.components import (
 from src.plcs.models.plcs_kp3d_model import PLCSKeypoint3DModel
 from src.plcs.models.plcs_model import PLCSModel
 from src.plcs.models.plcs_multiview_model import PLCSMultiViewModel
+from src.plcs.models.plcs_query_sequence_model import (
+    PLCSQuerySequenceModel,
+    build_plcs_sequence_model,
+)
 from src.plcs.models.plcs_sequence_model import PLCSSequenceModel
 
 __all__ = [
     "PLCSModel",
     "PLCSSequenceModel",
+    "PLCSQuerySequenceModel",
+    "build_plcs_sequence_model",
     "PLCSMultiViewModel",
     "PLCSKeypoint3DModel",
     "PerTokenKeypoint3DHead",
