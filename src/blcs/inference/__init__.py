5f9fd1ccf3fe764073eeb7a57785866e4fcb4455
Motoki0705 <kamimura098@gmail.com>
2026-02-10 21:52:20 +0900

    refactor: remove BLCSMultiViewPredictor from inference module exports

--- src/blcs/inference/__init__.py
+++ src/blcs/inference/__init__.py
@@ -1,8 +1,6 @@
 """BLCS inference modules."""
 
 from src.blcs.inference.predictor import BLCSPredictor
-from src.blcs.inference.multiview_predictor import BLCSMultiViewPredictor
 __all__ = [
     "BLCSPredictor",
-    "BLCSMultiViewPredictor"
 ]
